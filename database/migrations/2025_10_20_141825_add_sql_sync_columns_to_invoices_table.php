f26d541bc7f9e7f2085c224dc7bae4d840916049
Msiang <mou20181234@gmail.com>
2025-12-09 08:36:51 +0800

    update sql table name

--- database/migrations/2025_10_20_141825_add_sql_sync_columns_to_invoices_table.php
+++ database/migrations/2025_10_20_141825_add_sql_sync_columns_to_invoices_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('orders', function (Blueprint $table) {
+        Schema::table('invoices', function (Blueprint $table) {
             $table->string('sql_sync_status')->nullable()->after('status')->index();
             $table->text('sql_sync_respond')->nullable()->after('sql_sync_status');
         });
@@ -23,7 +23,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('orders', function (Blueprint $table) {
+        Schema::table('invoices', function (Blueprint $table) {
             $table->dropIndex(['sql_sync_status']);
             $table->dropColumn(['sql_sync_status', 'sql_sync_respond']);
         });
